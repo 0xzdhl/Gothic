8f9351271d79dbddeada0ef3540d0c26b59630f7
0xzdhl <266611332+0xzdhl@users.noreply.github.com>
2026-04-15 18:00:36 +0800

    feat: add type and select function.

--- src/trae/types.rs
+++ src/trae/types.rs
@@ -1,3 +1,6 @@
+use crate::trae::TraeEditor;
+use anyhow::Error;
+
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum TraeEditorMode {
     SOLO,
@@ -29,7 +32,7 @@ pub struct TraeTask {
     pub title: String,
     pub status: TraeTaskStatus,
     pub selected: bool,
-    pub index: usize
+    pub index: usize,
 }
 
 impl TraeTask {
@@ -50,5 +53,54 @@ impl TraeTask {
             self.status,
             TraeTaskStatus::Interrupted | TraeTaskStatus::Finished
         )
+    }
+}
+
+pub struct TraeTaskHandler<'a> {
+    editor: &'a TraeEditor,
+    snapshot: TraeTask,
+}
+
+impl<'a> TraeTaskHandler<'a> {
+    pub fn new(editor: &'a TraeEditor, snapshot: TraeTask) -> Self {
+        Self { editor, snapshot }
+    }
+
+    pub fn task(&self) -> &TraeTask {
+        &self.snapshot
+    }
+
+    pub fn title(&self) -> &str {
+        &self.snapshot.title
+    }
+
+    pub fn status(&self) -> TraeTaskStatus {
+        self.snapshot.status
+    }
+
+    pub fn index(&self) -> usize {
+        self.snapshot.index
+    }
+
+    pub fn is_selected(&self) -> bool {
+        self.snapshot.selected
+    }
+
+    pub fn is_finished(&self) -> bool {
+        self.snapshot.is_finished()
+    }
+
+    pub async fn refresh(&self) -> Result<TraeTaskHandler<'a>, Error> {
+        self.editor
+            .get_task_handle_by_index(self.snapshot.index)
+            .await
+    }
+
+    pub async fn select(&self) -> Result<(), Error> {
+        self.editor.select_task_by_index(self.index()).await
+    }
+
+    pub async fn type_content(&self, content: &str) -> Result<(), Error> {
+        self.editor.type_content_to_chat_input(content).await
     }
 }
