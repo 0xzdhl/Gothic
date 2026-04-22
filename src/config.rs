cae8f18a2986ff425d921863101698d1e5215825
0x0501 <41354084+0x0501@users.noreply.github.com>
2026-04-23 00:05:26 +0800

    feat: update configuration schema.

--- src/config.rs
+++ src/config.rs
@@ -3,9 +3,27 @@ use jsonc_parser::parse_to_serde_value;
 use serde::Deserialize;
 use std::fs;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
+pub struct ModelConfig {
+    #[allow(dead_code)]
+    api_key: String,
+    #[allow(dead_code)]
+    base_url: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum CommandStrategy {
+    Allow,
+    Deny,
+    LLM,
+}
+
+#[derive(Deserialize, Debug)]
 pub struct Config {
     pub trae_executable_path: String,
+    pub command_strategy: CommandStrategy,
+    pub model: ModelConfig,
 }
 
 impl Config {
